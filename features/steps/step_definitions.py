cde098ecf3575258c6ee52ccd31f1b12fb09d8c1
Szél Levente <szel.levente@diak.szbi-pg.hu>
2026-01-16 08:54:22 +0100

    változók bevezetése

--- features/steps/step_definitions.py
+++ features/steps/step_definitions.py
@@ -2,13 +2,10 @@ from behave import given, when, then
 from src.is_it_friday import is_it_friday
 
 
-@given('today is Sunday')
-def step_given_today_is(context):
-     context.today = "Sunday"
+@given('today is "{day}"')
+def step_given_today_is(context, day):
+     context.today = day
 
-@given('today is Friday')
-def step_given_today_is(context):
-     context.today = "Friday"
 
 @when("I ask whether it's Friday yet")
 def step_when_i_ask_whether_its_friday_yet(context):
