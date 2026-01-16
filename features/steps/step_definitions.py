83c1ae4fb7b4a3d417bb6cd8eb257a4a89b4115f
Szél Levente <szel.levente@diak.szbi-pg.hu>
2026-01-16 08:34:44 +0100

    save

--- features/steps/step_definitions.py
+++ features/steps/step_definitions.py
@@ -2,10 +2,13 @@ from behave import given, when, then
 from src.is_it_friday import is_it_friday
 
 
-@given('today is "{day}"')
-def step_given_today_is(context, day):
-    pass
+@given('today is Sunday')
+def step_given_today_is(context):
+     context.today = "Sunday"
 
+@given('today is Friday')
+def step_given_today_is(context):
+     context.today = "Friday"
 
 @when("I ask whether it's Friday yet")
 def step_when_i_ask_whether_its_friday_yet(context):
